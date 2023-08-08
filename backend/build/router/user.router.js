db53a63601b0c3270d59058484c3a92cce618ab1
HarrisK <haris6072@gmail.com>
2023-08-09 00:59:24 +0500

    removed build from backend

--- backend/build/router/user.router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const _controller_1 = require("@controller");
-const router = (0, express_1.Router)();
-router.post('/login', _controller_1.user.login);
-router.post('/signup', _controller_1.user.signup);
-router.post('/verify', _controller_1.user.verify);
-exports.default = router;
