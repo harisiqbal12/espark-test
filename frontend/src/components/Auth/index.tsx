a2ea1bec138a03a406b1fc8e7e18e0bf9ce4ca28
HarrisK <haris6072@gmail.com>
2023-08-09 17:35:50 +0500

    some changes

--- frontend/src/components/Auth/index.tsx
+++ frontend/src/components/Auth/index.tsx
@@ -36,8 +36,8 @@ export default function Auth(): JSX.Element {
 	return (
 		<div className='h-screen w-screen flex'>
 			<div className='w-1/2 h-full flex-col bg-fuchsia-500 flex p-20 text-white shrink-0'>
-				<h1 className='text-heading font-bold'>Todo</h1>
-				<span className='font-semibold -mt-4'>Todo by appiskey</span>
+				<h1 className='text-heading font-bold'>Test</h1>
+				<span className='font-semibold -mt-4'>Test by espark</span>
 			</div>
 
 			<Render />
